fff59da29e705e300b7e3b8c33e33d8418d93fcf
Christopher Sumnicht <csumnicht@berkeley.edu>
2014-10-04 03:56:18 -0700

    Added pseudo opcode stuff

--- app/src/main/java/com/example/christophersumnicht/txty/JavaToJSInterface.java
+++ app/src/main/java/com/example/christophersumnicht/txty/JavaToJSInterface.java
@@ -8,10 +8,73 @@ import android.webkit.JavascriptInterface;
 
 public class JavaToJSInterface {
 
+
+    //Series of pseudo "op-codes" to handle different button presses.
     @JavascriptInterface
-    public String getConsoleLine()
-    {
-        String consoleLine = ">";
-        return consoleLine;
+    public void addToConsoleAdd() {
+
+    }
+
+    @JavascriptInterface
+    public void addToConsoleMul() {
+
+    }
+
+    @JavascriptInterface
+    public void addToConsoleSub() {
+
+    }
+
+    @JavascriptInterface
+    public void addToConsoleDiv() {
+
+    }
+
+    @JavascriptInterface
+    public void addToConsoleEq() {
+
+    }
+
+    @JavascriptInterface
+    public void addToConsoleMap() {
+
+    }
+
+    @JavascriptInterface
+    public void addToConsoleFilter() {
+
+    }
+
+    @JavascriptInterface
+    public void addToConsoleReduce() {
+
+    }
+
+    @JavascriptInterface
+    public void addToConsoleMath() {
+
+    }
+
+    @JavascriptInterface
+    public void addToConsoleDraw() {
+
+    }
+
+    @JavascriptInterface
+    public void addToConsoleLambda() {
+
+    }
+
+    @JavascriptInterface
+    public void addToConsoleList() {
+
+    }
+
+    @JavascriptInterface
+    public void addToConsoleCond() {
+
+    }
+
+    public void flushLine() {
     }
 }
