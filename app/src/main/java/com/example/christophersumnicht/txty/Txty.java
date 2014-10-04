2d7d05b4ee221359435f4aa63afc7e0b4105e015
Eric Nguyen <neric@berkeley.edu>
2014-10-04 08:50:10 -0700

    Fixed a bunch of broken shit.

--- app/src/main/java/com/example/christophersumnicht/txty/Txty.java
+++ app/src/main/java/com/example/christophersumnicht/txty/Txty.java
@@ -6,6 +6,9 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.webkit.WebView;
 
+import org.jruby.embed.ScriptingContainer;
+import org.jruby.embed.LocalVariableBehavior;
+
 public class Txty extends Activity {
 
     public void onStart() {
@@ -14,6 +17,7 @@ public class Txty extends Activity {
         WebView webView =  (WebView)findViewById(R.id.mainView);
         //enable JavaScript
         webView.getSettings().setJavaScriptEnabled(true);
+        webView.addJavascriptInterface(new JavaScriptInterface(webView, this), "jsinterface");
         webView.loadUrl("file:///android_asset/index.html");
     }
 
@@ -41,5 +45,32 @@ public class Txty extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    // this bro does the code execution
+    final class JavaScriptInterface {
+        private ScriptingContainer rubyEnv;
+        private WebView webView;
+        private Activity activity;
+        JavaScriptInterface (WebView webView, Activity activity) {
+            this.webView = webView;
+            this.activity = activity;
+            rubyEnv = new ScriptingContainer(LocalVariableBehavior.PERSISTENT);
+        }
+
+        public String runCode(String code) {
+            System.out.println("Running:");
+            System.out.println(code);
+            System.out.println((String)(rubyEnv.runScriptlet(code)));
+            /*activity.runOnUiThread(new Runnable() {
+
+                @Override
+                public void run() {
+                    webView.loadUrl("javascript:printShit(\"Hello there\")");
+                }
+            });*/
+            webView.loadUrl("javascript:printShit(\"" + (String)(rubyEnv.runScriptlet(code)) + "\");");
+            return (String)(rubyEnv.runScriptlet(code));
+        }
     }
 }
